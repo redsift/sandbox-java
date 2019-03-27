95d1836bd1167b2a423d8acea3aa59d3913e8627
Christos Vontas <christos@redsift.io>
2019-03-27 12:42:53 +0000

    repurpose lookup test for get

--- test/server/Node1.java
+++ test/server/Node1.java
@@ -7,8 +7,9 @@ import java.nio.charset.StandardCharsets;
 public class Node1 {
     public static ComputeResponse compute(ComputeRequest req) throws Exception {
         System.out.println("Node1.java: " + req.toString());
-        System.out.println(req.lookup[0].data.key);
-        System.out.println(new String(req.lookup[0].data.value, StandardCharsets.UTF_8));
+        System.out.println(req.get[0].data);
+        System.out.println(req.get[0].data[0].key);
+        System.out.println(new String(req.get[0].data[0].value, StandardCharsets.UTF_8));
         ComputeResponse res = new ComputeResponse("bucket", "key", "value", 0);
         return res;
     }
